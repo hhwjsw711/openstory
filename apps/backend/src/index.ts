f6f3fe36631266bffc67933e8bc9ec6de5418231
Tom Beckenham <34339192+tombeckenham@users.noreply.github.com>
2025-10-14 18:41:30 +1100

    102-task-restructure-repo-to-setup-elysia (#103)

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,9 @@
+import { Elysia } from "elysia";
+
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .listen(process.env.PORT ?? 3030);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+);
