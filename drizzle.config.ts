6533c208584e5e6e816ac945b1c6a4ee42b235b3
Tom Beckenham <34339192+tombeckenham@users.noreply.github.com>
2025-10-27 16:21:19 +1100

    Changed to use non pooling url for drizzle kit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'drizzle-kit';
 
-const POSTGRES_URL = process.env.POSTGRES_URL;
+const POSTGRES_URL = process.env.POSTGRES_URL_NON_POOLING;
 if (!POSTGRES_URL) {
   throw new Error('POSTGRES_URL environment variable is not set');
 }
