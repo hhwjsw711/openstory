de3732a805f6e4612faee28758e98257706def12
Tom Beckenham <34339192+tombeckenham@users.noreply.github.com>
2025-10-27 17:24:23 +1100

    Updated config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,20 +1,14 @@
 import { defineConfig } from 'drizzle-kit';
 
-console.log({
-  dbCredentials: {
-    url: process.env.POSTGRES_NON_POOLING_URL!,
-  },
-});
+const dbUrl = new URL(process.env.POSTGRES_URL_NON_POOLING!);
+dbUrl.searchParams.set('sslmode', 'no-verify');
 
 export default defineConfig({
   schema: './src/lib/db/schema/index.ts',
   out: './drizzle/migrations',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.POSTGRES_URL_NON_POOLING!,
-    ssl: {
-      ca: process.env.DATABASE_CERTIFICATE!,
-    },
+    url: dbUrl.toString(),
   },
   verbose: true,
   strict: true,
