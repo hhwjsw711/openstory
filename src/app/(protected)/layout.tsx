ec4f112e23bb636b1f842a3fe647b2e2085efd48
Tom Beckenham <34339192+tombeckenham@users.noreply.github.com>
2025-11-19 16:55:43 +1100

    Changed default layout to be a server component

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,9 +1,20 @@
 import { AppLayout } from '@/components/layout';
+import { auth } from '@/lib/auth/config';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 
-export default function ProtectedLayout({
+export default async function ProtectedLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    return redirect('/login');
+  }
+
   return <AppLayout>{children}</AppLayout>;
 }
